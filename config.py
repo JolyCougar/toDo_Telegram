c3fb3546a54fef094eeec6abc8fc3e278e0d54de
Yuri <Jolycougar@gmail.com>
2024-10-23 12:02:58 +0300

    add:func to view tasks(not compete)

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-API_TOKEN = '7851220576:AAGw5G8L__b0RnHy3DXfNQLbUl6LZnvv8l8'  # Замените на токен вашего бота
+API_TOKEN = ''  # Замените на токен вашего бота
 DJANGO_API_URL = 'http://127.0.0.1:8000/api/v1/'  # Замените на URL вашего Django API
-DB_HOST = 'localhost'
-DB_NAME = 'telegram'
-DB_USER = 'postgres'
-DB_PASSWORD = '123456'
+DB_HOST = ''
+DB_NAME = ''
+DB_USER = ''
+DB_PASSWORD = ''
