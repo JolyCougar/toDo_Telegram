eb7cd73b39c8c0fa507029d9f4b4f75bb77bf4c0
Yuri <Jolycougar@gmail.com>
2024-10-27 22:07:01 +0300

    add: local func in bot

--- handlers/handle_local.py
+++ handlers/handle_local.py
@@ -3,8 +3,6 @@ from telegram import Update
 from services.db import Session_local, set_local_mode
 from .start_handler import send_main_keyboard
 
-local_mode = False
-
 
 async def handle_local_mode(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     user_id = update.message.from_user.id
